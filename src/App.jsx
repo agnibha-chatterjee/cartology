80f322ff4d696899a1f6becd7ad6df216b95ae54
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-03-01 13:45:19 -0800

    init commit with firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CreateProduct } from './components/CreateProduct';
+import { Jumbotron } from './components/Jumbotron';
+
+function App() {
+  return (
+    <div>
+      <Jumbotron />
+      <CreateProduct />
+    </div>
+  );
+}
+
+export default App;
